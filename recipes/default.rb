3ebf6fa206c51fdcc5f157db6b9e33b4363096ae
COMTECHIES <CLOUDTECHPROJECT@GMAIL.COM>
2013-12-03 17:26:05 +0530

    asd
    asd

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: travisdemo
+# Cookbook Name:: apache2
 # Recipe:: default
 #
 # Copyright 2013, YOUR_COMPANY_NAME
@@ -7,7 +7,33 @@
 # All rights reserved - Do Not Redistribute
 #
 
-directory #node['asdfjygasd']['asdasdasd'] do
-  action :create
-  askdhfkjasdf askdj f isagd fj
+ package 'apache2' do
+   action :nothing
+  
+ end
+ 
+ package 'unzip' do
+   
+ end
+ 
+ cookbook_file '/var/www/site.zip' do
+    source 'site.zip'
+    mode   '0755'
+    owner  'root'
+    group  node['apache']['root_group']
+ end
+ 
+ bash "extracting files" do
+   cwd "/var/www"
+   code <<-EOH
+   unzip site.zip 
+   EOH
+   not_if {File.exists?("/var/www/images")}
+ end 
+ 
+template "/var/www/index.html" do
+    source "index.html.erb"
+    mode  '0644'
+    owner 'root'
+    group node['apache']['root_group']
 end
